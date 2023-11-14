b5038a3032b5e86b7b06da29cc2216f101b2ab19
Uvejsii <136826106+Uvejsii@users.noreply.github.com>
2023-11-14 23:41:10 +0100

    Add files via upload
    Initial commit.

---
+++ main.js
@@ -0,0 +1,6 @@
+let shareContainer = document.querySelector('.hide');
+let shareButton = document.querySelector('.share-btn');
+
+shareButton.addEventListener('click', () => {
+    shareContainer.classList.toggle('hide');  
+}) 
